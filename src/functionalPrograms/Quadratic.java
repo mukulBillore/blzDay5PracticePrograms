9c12e610c52e199a2ba77625ed59a634344317db
mukulBillore <mukulbillore88@gmail.com>
2021-12-12 23:12:33 +0530

    modified : file in  fun programs

--- src/functionalPrograms/Quadratic.java
+++ src/functionalPrograms/Quadratic.java
@@ -1,10 +1,16 @@
 package functionalPrograms;
 
+import java.util.Scanner;
+
 public class Quadratic {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt(),b=sc.nextInt(),c=sc.nextInt();
+		double delta=b*b - 4*a*c;
+		double x1 = (-b + Math.sqrt(delta))/(2*a);
+		double x2 = (-b - Math.sqrt(delta))/(2*a);
+		System.out.println("the roots of the quadratic equations are : "+x1+","+x2);
 	}
 
 }
