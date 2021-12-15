a5048f4bb0e406bc6818c865cab88e97a6803511
mukulBillore <mukulbillore88@gmail.com>
2021-12-15 08:23:02 +0530

    Added : new workshop package

---
+++ src/workshopBlz/Gambler.java
@@ -0,0 +1,14 @@
+package workshopBlz;
+
+public class Gambler {
+	//uc1
+	public static void oneDay() {
+		int everyDayStake =100;
+		int everyGameBet =1;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
